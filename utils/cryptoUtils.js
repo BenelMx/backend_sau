2d39057a26e94f7f821ee86da57b2d2d06b0efbe
BenelIT <benelmx@proton.me>
2024-12-23 14:56:43 -0600

    Mejor incluso

--- utils/cryptoUtils.js
+++ utils/cryptoUtils.js
@@ -1,1 +1,15 @@
 // utils\cryptoUtils.js
+
+const crypto = require('crypto');
+
+function encryptData(data) {
+    const algorithm = 'aes-256-cbc';  // Algoritmo de encriptación
+    const key = crypto.randomBytes(32);  // Clave secreta (debería ser almacenada de forma segura)
+    const iv = crypto.randomBytes(16);  // Vector de inicialización
+    const cipher = crypto.createCipheriv(algorithm, key, iv);
+
+    let encrypted = cipher.update(data, 'utf8', 'hex');
+    encrypted += cipher.final('hex');
+
+    return { encryptedData: encrypted, iv: iv.toString('hex'), key: key.toString('hex') };
+}
